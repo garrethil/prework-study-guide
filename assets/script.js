086be03c9bb39bf0c99c5be8078508fe7298f5a2
garrethil <garrethildebrandt@gmail.com>
2023-10-05 15:24:41 -0400

    added functions in js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
-var topic = ["HTML", "CSS", "Git", "JavaScript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
 
-for(var x = 0; x < topic.length; x++)
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-console.log(topic[x])
+console.log("Heare are the topics we learned through Prework:");
+function listTopics() {
+    for(var x = 0; x < topics.length; x++){
+        console.log(topics[x])
+    }
 
-/*
-if (topic[x] === 'HTML') {
+}
+
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic[x] === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic[x] === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic[x] === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
-*/
+}
+listTopics()
+
+console.log("Which topic should we study first?");
+selectTopic()
+
